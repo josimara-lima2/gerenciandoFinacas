cc8ec5f689383f04ca532d773996f218963aba40
Josimara <josybehlul@gmail.com>
2021-12-27 17:42:12 -0300

    feat: add image home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 import { Box } from '@mui/material';
+import welcome from '../assets/images/welcome.png';
 
 export default function Home() {
-  return <Box>Home</Box>;
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <img width="40%" src={welcome} alt="welcome" />
+    </Box>
+  );
 }
